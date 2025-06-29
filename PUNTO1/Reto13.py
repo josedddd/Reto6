794a86bda23d63becfc399eb5fb0845c4ca8f48d
josedddd <144506601+josedddd@users.noreply.github.com>
2025-06-28 22:09:59 -0500

    Update Reto13.py

--- PUNTO1/Reto13.py
+++ PUNTO1/Reto13.py
@@ -1,5 +1,6 @@
-#Si la lista de entrada contiene otro tipo de dato, que no sea un INT, va a detectar el error y lo va a "guardar", lo interesante es que  el programa no genera error
+#Si la lista de entrada contiene otro tipo de dato, que no sea un int, va a detectar el error y lo va a "guardar", lo interesante es que el programa no genera error
 # y aunque exista un dato incorrecto, sigue sacando los numeros enteros que son primos. (Try-except en su totalidad)
+
 def prime(list_num: list):
     prime_num=[]  
     for num in list_num:
